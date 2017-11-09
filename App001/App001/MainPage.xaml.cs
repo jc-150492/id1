487d4441091f0f8bb5afcaebf4bf561dfaff8e79
150492 <150492@E0505-06>
2017-11-09 16:10:03 +0900

    初回

--- App001/App001/MainPage.xaml.cs
+++ App001/App001/MainPage.xaml.cs
@@ -13,5 +13,42 @@ namespace App001
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Double h = Double.Parse(this.He.Text);
+            Double w = Double.Parse(this.We.Text);
+
+
+            h = h / 100;
+
+            double bmi = w / (h * h);
+
+            String st;
+            if (bmi < 18.5)
+            {
+                st = "痩せています。";
+            }
+            else if (bmi < 25)
+            {
+                st = "標準体重です";
+            }
+            else if (bmi < 30)
+            {
+                st = "太り気味です";
+            }
+            else if (bmi < 35)
+            {
+                st = "太っています。";
+            }
+            else
+            {
+                st = "太りすぎです。";
+            }
+
+            DisplayAlert("BIM数値", st, "OK");
+
+
+        }
     }
 }
